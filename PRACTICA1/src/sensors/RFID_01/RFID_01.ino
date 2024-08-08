4e0b0e7f9e2a6d939a1f146c0fc06751e585567c
allestgg <allanestuard0sb@gmail.com>
2024-08-08 01:04:36 -0600

    RFID SENSOR v2

--- PRACTICA1/src/sensors/RFID_01/RFID_01.ino
+++ PRACTICA1/src/sensors/RFID_01/RFID_01.ino
@@ -1,11 +1,11 @@
 #include <SPI.h>
 #include <MFRC522.h>
-#include "User.h" // Incluye el archivo de encabezado para la clase User
+#include "User.h" 
 
 #define RST_PIN 9                         //Pin 9 para el reset del RC522
 #define SS_PIN 10                         //Pin 10 para el SS (SDA) del RC522
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);         //Creamos el objeto para el RC522
+MFRC522 mfrc522(SS_PIN, RST_PIN);         
 
 User user1("860FA022");                   //Se crea un objeto de tipo usuario con su pin correspondiente
 User user2("D46BE373");                   //Se crea un objeto de tipo usuario con su pin correspondiente
@@ -19,34 +19,17 @@ void setup() {
 }
 
 void loop() {
-  if (mfrc522.PICC_IsNewCardPresent()) {  //Leemos si hay una tarjeta presente
-
-  /*
-      Propósito: Intenta leer el número de serie (UID) de la tarjeta RFID actual.
-      Retorno: Devuelve true si se ha leído correctamente el UID de la tarjeta y false si ha fallado en la lectura.
-      Cómo Funciona: Cuando esta función es llamada, el lector intenta obtener la información de la tarjeta que 
-      está cerca. Si la lectura es exitosa, la información de la tarjeta (como el UID) se almacena en el objeto mfrc522.
-  */
-
-    if (mfrc522.PICC_ReadCardSerial()) {  // Enviamos serialemente su UID
+  if (mfrc522.PICC_IsNewCardPresent()) {  
+    if (mfrc522.PICC_ReadCardSerial()) {  
       Serial.print("Card UID:");    
-      pin = "";                           // Limpiar el pin antes de llenarlo nuevamente
-      
-      // La direccion se guarda en un array de bytes, que imprimimos uno u otro utilizando operador ternario
+      pin = "";                           
+
       for (byte i = 0; i < mfrc522.uid.size; i++) {
-        
-        //Se valida que se imprima un 0 acompanado de un numero si es menor a 16 para siempre tener
-        //el formato de 4 pares, si el numero es mayor a 16 solo imprimimos un espacio para tener
-        //separados los pares
-        Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-        //Accedo a la posicion i del arreglo y lo imprimo en formato hexadecimal
-        Serial.print(mfrc522.uid.uidByte[i], HEX);
-        
         pin += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
         pin += String(mfrc522.uid.uidByte[i], HEX);
       }
 
-      pin.toUpperCase(); // Asegúrate de que el pin esté en mayúsculas para la comparación
+      pin.toUpperCase(); 
 
       if (pin.equals(user1.getPin())) {
         user1.setState(true);

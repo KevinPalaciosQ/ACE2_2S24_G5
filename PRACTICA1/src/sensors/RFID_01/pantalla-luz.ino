6507f47f42be45278efeb2784f50f045086dcf7e
KevinPalaciosQ <95326781+KevinPalaciosQ@users.noreply.github.com>
2024-08-20 23:25:14 -0600

    Update pantalla-luz.ino

--- PRACTICA1/src/sensors/RFID_01/pantalla-luz.ino
+++ PRACTICA1/src/sensors/RFID_01/pantalla-luz.ino
@@ -33,3 +33,30 @@ void loop()
   delay(2000);
   lcd_1.clear();
 }
+#SENSOR DHT11
+#include <DHT.h>
+
+#define DHTTYPE DHT11
+#define DHTPIN 9
+
+DHT dht(DHTPIN,DHTTYPE);
+
+void setup(){
+    Serial.begin(9600);
+    dht.begin();
+}
+
+void loop(){
+    delay(2000);
+    float humidity = dht.readHUmidity();
+    float temperature = dht.readTemperature();
+    if(isnan(humidity)||isnan(temperature)){
+        Serial.println("ERROR EN EL SENSOR ");
+        return
+    }
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("% Temperature: ");
+    Serial.println(temperature);
+    Serial.println("°C")
+}

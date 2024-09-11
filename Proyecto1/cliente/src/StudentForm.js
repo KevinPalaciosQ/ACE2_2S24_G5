63f9dcc1478d4caa09a68a069d986d939b6ca43b
KevinPalaciosQ <3001877180101@ingenieria.usac.edu.gt>
2024-09-10 22:14:13 -0600

    update students

--- Proyecto1/cliente/src/StudentForm.js
+++ Proyecto1/cliente/src/StudentForm.js
@@ -3,37 +3,41 @@ import './StudentForm.css';
 
 const StudentForm = () => {
   return (
-    <div className="student-form-container">
-      <div className="photo-container">
-        <img
-          src="https://via.placeholder.com/150" // URL de la imagen (puedes reemplazarla con la foto deseada)
-          alt="Student"
-          className="student-photo"
-        />
-      </div>
-      <div className="form-content">
-        <h2>Ingreso de Estudiantes</h2>
-        <form className="form-container">
-          <label>
-            First Name:
-            <input type="text" name="firstName" />
-          </label>
-          <label>
-            Last Name:
-            <input type="text" name="lastName" />
-          </label>
-          <label>
-            UID:
-            <input type="text" name="uid" />
-          </label>
-          <label>
-            Model:
-            <input type="text" name="model" />
-          </label>
-        </form>
-        <div className="form-buttons">
-          <button type="button" className="prev-button">Anterior</button>
-          <button type="button" className="next-button">Siguiente</button>
+    <div className="card-container">
+      <div className="student-form-card">
+        <div className="student-form-content">
+          <div className="photo-container">
+            <img
+              src="https://via.placeholder.com/150" // URL de la imagen (puedes reemplazarla con la foto deseada)
+              alt="Student"
+              className="student-photo"
+            />
+          </div>
+          <div className="form-content">
+            <h2>Ingreso de Estudiantes</h2>
+            <form className="form-container">
+              <label>
+                First Name:
+                <input type="text" name="firstName" disabled />
+              </label>
+              <label>
+                Last Name:
+                <input type="text" name="lastName" disabled />
+              </label>
+              <label>
+                UID:
+                <input type="text" name="uid" disabled />
+              </label>
+              <label>
+                Model:
+                <input type="text" name="model" disabled />
+              </label>
+            </form>
+            <div className="form-buttons">
+              <button type="button" className="prev-button">Anterior</button>
+              <button type="button" className="next-button">Siguiente</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>

5a78f1dfd3243c85aed4594fe7ee9915e8a761d5
KevinPalaciosQ <3001877180101@ingenieria.usac.edu.gt>
2024-09-10 21:26:59 -0600

    update ingreso Estudiantes

--- Proyecto1/cliente/src/StudentForm.js
+++ Proyecto1/cliente/src/StudentForm.js
@@ -1,27 +1,41 @@
 import React from "react";
+import './StudentForm.css';
 
 const StudentForm = () => {
   return (
-    <div>
-      <h2>Ingreso de Estudiantes</h2>
-      <form className="form-container">
-        <label>
-          First Name:
-          <input type="text" name="firstName" />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" name="lastName" />
-        </label>
-        <label>
-          UID:
-          <input type="text" name="uid" />
-        </label>
-        <label>
-          Model:
-          <input type="text" name="model" />
-        </label>
-      </form>
+    <div className="student-form-container">
+      <div className="photo-container">
+        <img
+          src="https://via.placeholder.com/150" // URL de la imagen (puedes reemplazarla con la foto deseada)
+          alt="Student"
+          className="student-photo"
+        />
+      </div>
+      <div className="form-content">
+        <h2>Ingreso de Estudiantes</h2>
+        <form className="form-container">
+          <label>
+            First Name:
+            <input type="text" name="firstName" />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" name="lastName" />
+          </label>
+          <label>
+            UID:
+            <input type="text" name="uid" />
+          </label>
+          <label>
+            Model:
+            <input type="text" name="model" />
+          </label>
+        </form>
+        <div className="form-buttons">
+          <button type="button" className="prev-button">Anterior</button>
+          <button type="button" className="next-button">Siguiente</button>
+        </div>
+      </div>
     </div>
   );
 };

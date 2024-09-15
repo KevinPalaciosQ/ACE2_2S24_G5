f4a3af4072936de63e5ee5a0e93fc837ebc44b98
KevinPalaciosQ <3001877180101@ingenieria.usac.edu.gt>
2024-09-14 23:47:43 -0600

    función de login y front actualizados

--- Proyecto1/cliente/src/Login.js
+++ Proyecto1/cliente/src/Login.js
@@ -1,52 +1,51 @@
 import React, { useState } from "react";
 import "./Login.css";
+import { FaUser, FaLock } from "react-icons/fa";
 
 const Login = ({ onLogin }) => {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = (e) => {
     e.preventDefault();
-    if (email === "administrador@administrador" && password === "administrador") {
-      console.log("Email:", email);
+    if (username === "administrador@administrador" && password === "administrador") {
+      console.log("Username:", username);
       console.log("Password:", password);
       alert("Inicio de sesión exitoso");
-      onLogin();  // Llama a la función para cambiar el estado y redirigir
+      onLogin(); // Realización del login
     } else {
       alert("Credenciales incorrectas");
     }
   };
 
   return (
-    <div className="login-container">
-      <div className="login-card">
-        <h2>Iniciar Sesión</h2>
-        <form onSubmit={handleLogin}>
-          <div className="input-group">
-            <label htmlFor="email">Correo electrónico</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="administrador@administrador"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Contraseña</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="administrador"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button className="login-btn" type="submit">Ingresar</button>
-        </form>
-      </div>
+    <div className="wrapper">
+      <form onSubmit={handleLogin}>
+        <h1>Login</h1>
+        <div className="input-box">
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <FaUser className="icon" />
+        </div>
+
+        <div className="input-box">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <FaLock className="icon" />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };

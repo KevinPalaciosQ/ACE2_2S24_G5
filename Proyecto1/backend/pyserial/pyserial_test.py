6e9fb88b4b8f8eaa56ab9bf3caa8eb1010e57b65
allan502 <allanestuard0sb@gmail.com>
2024-09-30 15:23:32 -0600

    Pyserial test with arduino 02

--- Proyecto1/backend/pyserial/pyserial_test.py
+++ Proyecto1/backend/pyserial/pyserial_test.py
@@ -1,10 +1,20 @@
-import serial, time
+import serial
+import json
 
 ard_object = serial.Serial('COM3', 9600)
 
 data = ard_object.readline()
 
 while True:
-    print(data)
-    time.sleep(1)
-    data = ard_object.readline()
+    if ard_object.in_waiting > 0:
+        data = ard_object.readline().decode('utf-8').strip()
+        try:
+            id, temperature, humidity = data.split(',')
+            data_json = {
+                "id": id,
+                "temperature": temperature,
+                "humidity": humidity
+            }
+            print(json.dumps(data_json, indent=4))
+        except ValueError:
+            print(f"Error al procesar los datos: {data}")
